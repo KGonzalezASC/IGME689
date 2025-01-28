b1dd0e9dddc234c3e5020e65c1d522bdb9695677
Aaron Bush (RIT Student) <aob6548@rit.edu>
2025-01-28 18:02:19 -0500

    Moved code interfacing with Jolt into Physics Class

--- Material.cpp
+++ Material.cpp
@@ -58,13 +58,8 @@ void Material::PrepareMaterial(std::shared_ptr<Transform> transform, std::shared
     vertexShader->SetShader();
     pixelShader->SetShader();
 
-    // Check if transform is dirty or it's the first frame
-    if (transform->isDirtyWorld() || firstPass)
-    {
-        vertexShader->SetMatrix4x4("world", transform->getWorldMatrix());
-        printf("World matrix is dirty (or first pass)\n");
-    }
 
+    vertexShader->SetMatrix4x4("world", transform->getWorldMatrix());
     // View and projection matrices are likely per-frame and always need to be set
     vertexShader->SetMatrix4x4("view", camera->getViewMatrix());
     vertexShader->SetMatrix4x4("projection", camera->getProjectionMatrix());
